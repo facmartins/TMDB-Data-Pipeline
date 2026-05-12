fe7e66fae42224f6d8f823907e125594b78cb147
Flávia de Castro <flaviadecastro.projects@gmail.com>
2026-05-12 09:17:45 +0100

    Update create_tables_sql.sql

--- create_tables_sql.sql
+++ create_tables_sql.sql
@@ -1,5 +1,5 @@
--- Criar a tabela Filmes
-CREATE TABLE Filmes (
+-- Create table
+CREATE TABLE Movies (
     id INT IDENTITY(1,1) PRIMARY KEY,
     title NVARCHAR(255) NOT NULL,
     release_date DATE,
